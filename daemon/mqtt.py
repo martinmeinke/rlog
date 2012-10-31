934b8a5e1b05d43d38034b2a9902f02f9368bbac
Stephan Wypler <s.wypler@online.de>
2012-10-31 15:03:35 +0100

    mqtt - ready?

--- daemon/mqtt.py
+++ daemon/mqtt.py
@@ -27,10 +27,37 @@ class mqtt():
         else:
             self.__pendingUnsubscriptionsList.append(topic)
             print "Client is not connected at the moment. Will unsubscribe on connection"
+            
+    def publish(self, topic, message):
+        self._client.publish(topic, message)
     
+    # override this methods to add your functionality
+    def on_connect(self, rc):
+#        print "on_connect", rc
+        pass
+    
+    def on_disconnect(self):
+#        print "on_disconnect"
+        pass
+            
+    def on_subscribe(self, mid, qos_list):
+#        print "on_subscribe", mid, qos_list
+        pass
+        
+    def on_unsubscribe(self, mid):
+#        print "on_unsubscribe", mid
+        pass
+    
+    def on_publish(self, mid):
+#        print "on_publish", mid
+        pass
+            
+    def on_message(self, msg):
+#        print "Message received on topic " + msg.topic + " with QoS " + str(msg.qos) + " and payload " + msg.payload
+        pass
 
-    #MQTT callback
-    def on_connect(self, mosquitto_instance, rc):
+    # actual MQTT callbacks
+    def __on_connect(self, mosquitto_instance, rc):
         #rc 0 successful connect
         if rc == 0:
             print "MQTT Connected"
@@ -43,22 +70,27 @@ class mqtt():
                 topic = self.__pendingUnsubscriptionsList.pop()
                 print "executing pending unsubscription from " + topic
                 self.unsubscribe(topic)
+            self.on_connect(rc)
+        else:
+            print "We have an error here. Cleaning up ..."
+            self.__cleanup()
                   
-    def on_disconnect(self, rc):
+    def __on_disconnect(self, mosquitto_instance):
         self.__connected = False
-        print "Disconnected successfully." 
+        print "Disconnected successfully."
+        self.on_disconnect()
             
-    def on_subscribe(self, mosquitto_instance, mid, qos_list):
-        pass
-        
-    def on_unsubscribe(self,mosquitto_instance, mid):
-        pass
+    def __on_subscribe(self, mosquitto_instance, mid, qos_list):
+        self.on_subscribe(mid, qos_list)
     
-    def on_message(self, msg):
-        print "Message received on topic " + msg.topic + " with QoS " + str(msg.qos) + " and payload " + msg.payload
+    def __on_unsubscribe(self,mosquitto_instance, mid):
+        self.on_unsubscribe(mid)
     
-    def __on_message(self, mosquitto_instance, msg):
-        self.on_message(msg)
+    def __on_publish(self, mosquitto_instance, mid):
+        self.on_publish(mid)
+    
+    def __on_message(self, mosquitto_instance, message):
+        self.on_message(message)
 
     #called on exit
     # disconnect MQTT and clean up subscriptions
@@ -78,24 +110,21 @@ class mqtt():
             client_uniq = "rlog" + str(random.randint(0, 1000))
             self._client = mosquitto.Mosquitto(client_uniq)
             
-
             #attach MQTT callbacks
-            self._client.on_connect = self.on_connect
-            self._client.on_disconnect = self.on_disconnect
+            self._client.on_connect = self.__on_connect
+            self._client.on_disconnect = self.__on_disconnect
+            self._client.on_subscribe = self.__on_subscribe
+            self._client.on_unsubscribe = self.__on_unsubscribe
+            self._client.on_publish = self.__on_publish
             self._client.on_message = self.__on_message
-            self._client.on_subscribe = self.on_subscribe
-            self._client.on_unsubscribe = self.on_unsubscribe
 
             #connect to broker
             self._client.connect(self.__broker, self.__port, 60, True)
-            #remain connected to broker
+            
+            #remain connected to broker 
+              # stupid that it doesn't work w/o that silly loop
             thread.start_new_thread(self.loop, ())
     
-        # handle app closure
-        except (KeyboardInterrupt):
-            print "Interrupt received"
-            self.__cleanup()
-        
         except (RuntimeError):
             print "uh-oh! time to die"
             self.__cleanup()
--- daemon/test.py
+++ daemon/test.py
@@ -7,7 +7,7 @@ test.subscribe("test", 0)
 def foo(message):
   print "and here it comes", message.payload
 
-time.sleep(10)
+time.sleep(3)
 print "changed message callback"
 test.on_message = foo
 time.sleep(1)
@@ -17,6 +17,7 @@ test.unsubscribe("foo")
 try:
   while(True):
     time.sleep(1)
+   # test.publish("time", time.now())
 
 # handle app closure
 except (KeyboardInterrupt):
