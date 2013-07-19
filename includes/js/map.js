b011a19137db284403070671dad8eadb838ad868
Stephan Wypler <s.wypler@online.de>
2013-07-19 21:30:01 +0200

    added the map

--- includes/js/map.js
+++ includes/js/map.js
@@ -3,17 +3,52 @@ function RlogInstance(){
 }; 
  
 /* BASE APPLICATION LOGIC & MQTT EVENT HANDLING */
-function Map(){
+function Map(containerName){
     var self = this;
     this.mqttClient = undefined;
     this.RlogInstances = {};
+    this.openlayersMap = undefined;
+    this.initOpenLayersMap = function(containerName){
+        OpenLayers.ImgPath = "/static/img/OpenLayers/";
+        this.openlayersMap = new OpenLayers.Map(document.getElementById(containerName), {allOverlays: true, theme: "/static/css/OpenLayers/style.css"});
+        this.openlayersMap.addControl(new OpenLayers.Control.LayerSwitcher());
+        
+        // the SATELLITE layer has all 22 zoom level, so we add it first to
+        // become the internal base layer that determines the zoom levels of the
+        // map.
+        var gsat = new OpenLayers.Layer.Google(
+            "Google Satellite",
+            {type: google.maps.MapTypeId.SATELLITE, numZoomLevels: 22, visibility: false}
+        );
+        var gphy = new OpenLayers.Layer.Google(
+            "Google Physical",
+            {type: google.maps.MapTypeId.TERRAIN, visibility: false}
+        );
+        var gmap = new OpenLayers.Layer.Google(
+            "Google Streets", // the default
+            {numZoomLevels: 20, visibility: false}
+        );
+        var ghyb = new OpenLayers.Layer.Google(
+            "Google Hybrid",
+            {type: google.maps.MapTypeId.HYBRID, numZoomLevels: 22, visibility: true}
+        );
+
+        this.openlayersMap.addLayers([ghyb, gsat, gphy, gmap]);
+
+        // Google.v3 uses EPSG:900913 as projection, so we have to
+        // transform our coordinates
+        this.openlayersMap.setCenter(new OpenLayers.LonLat(10.2, 48.9)
+                                    .transform(new OpenLayers.Projection("EPSG:4326"), 
+                                               this.openlayersMap.getProjectionObject())
+                                , 5);
+    };
     this.reconnect = function(){
       console.log("Reconnecting");
       self.disconnect();
       self.connect(); 
     };
     this.connect = function(){
-      self.mqttClient = new Messaging.Client("192.168.8.157", 18883, "rlog-web-"+Math.random().toString(36).substring(6));
+      self.mqttClient = new Messaging.Client("localhost", 18883, "rlog-web-"+Math.random().toString(36).substring(6));
       self.mqttClient.onConnectionLost = self.connectionLost;
       self.mqttClient.onMessageArrived = self.messageArrived;
       self.mqttClient.connect({onSuccess:self.connected});
@@ -66,7 +101,14 @@ function Map(){
       } else if(topic[3] == "meta"){                                 
         instance[topic[4]] = payload;
       }
+      console.log("this is the new / updated one:", instance);
     };
+    this.initOpenLayersMap(containerName);
 };
-var map = new Map();
-map.connect();
+
+var map = undefined;
+$(document).ready(function() {
+    map = new Map('mapDiv');
+    map.connect();
+});
+
