a3f84aaa774c269f85871e38be2caa1524ddc22f
Stephan Wypler <s.wypler@online.de>
2014-04-02 23:18:35 +0200

    start coding

---
+++ daemon/cpp/src/rlogd.cpp
@@ -0,0 +1,11 @@
+extern "C" {
+    #include <MQTTAsync.h>
+}
+#include <getopt_pp.h>
+#include <sqlite3.h>
+#include <SerialStream.h>
+#include <cstdlib>
+
+int main(int argc, char* argv[]){
+	exit(0);
+}
