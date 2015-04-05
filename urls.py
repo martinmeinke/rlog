74ab54b1b556c299354ddf2d704c8ad321aff564
Stephan Wypler <s.wypler@online.de>
2015-04-06 01:38:29 +0200

    removed patterns() from urls.py because it will be deprecated anyway soon

--- urls.py
+++ urls.py
@@ -1,4 +1,4 @@
-from django.conf.urls import patterns, url, include
+from django.conf.urls import url, include
 from django.views.generic import RedirectView
 from django.conf import settings
 
@@ -7,12 +7,12 @@ if settings.ADMIN_ENABLED:
     from django.contrib import admin
     admin.autodiscover()
 
-urlpatterns = patterns('',
+urlpatterns = [
     url(r'^$', RedirectView.as_view(url='charts/live')),
     url(r'^charts/', include('charts.urls')),
-)
+]
 
 if settings.ADMIN_ENABLED:
-    urlpatterns = patterns('',
+    urlpatterns += [
         url(r'^admin/', include(admin.site.urls)),
-    )
+    ]
